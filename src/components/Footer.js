1297f3720499162d1dfe6ed413ddebb8f6ea6bc2
21929457 <apple855005@gmail.com>
2024-09-04 10:21:49 +0900

    푸터 내용 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,29 @@
+import '../css/userBottom.scss';
+import logoimg from '../images/user/logo.png';
+
 function Footer() {
     return (
-      <div></div>
+      <footer>
+        <div className="f-text-wrap">
+          <div className="footer-l">
+            <div className="logo">
+              <img src={logoimg}></img>
+              <p>대구대학교 정보통신대학 컴퓨터공학과</p>
+            </div>
+            <div className="footer-l-tit">
+              <p>정보통신대학장</p>
+              <p>김순철</p>
+            </div>
+            <div className="footer-info">
+              <p><span>Location</span>경상북도 경산시 진량읍 대구대로 201 정보통신대학 컴퓨터정보공학부 컴퓨터공학전공</p>
+              <p><span>TEL</span>053-850-6570</p>
+            </div>
+          </div>
+          <div className="footer-r">
+
+          </div>
+        </div>
+      </footer>
     );
   }
   
