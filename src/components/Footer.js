321d64395cd8cf14bf2bde81f19196b8181c82c0
21929457 <apple855005@gmail.com>
2024-09-04 14:27:57 +0900

    footer 수정 완료

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,23 +4,19 @@ import logoimg from '../images/user/logo.png';
 function Footer() {
     return (
       <footer>
-        <div className="f-text-wrap">
-          <div className="footer-l">
+        <div className="ft-text-wrap">
+          <div className="ft-inner">
             <div className="logo">
               <img src={logoimg}></img>
               <p>대구대학교 정보통신대학 컴퓨터공학과</p>
             </div>
-            <div className="footer-l-tit">
-              <p>정보통신대학장</p>
-              <p>김순철</p>
+            <p className="ft-tit">정보통신대학장 교수 김순철</p>
+            <div className="ft-info">
+              <div>
+                <p><span>Location : &nbsp;</span>경상북도 경산시 진량읍 대구대로 201 정보통신대학 컴퓨터정보공학부 컴퓨터공학전공</p>
+                <p><span>TEL : &nbsp;</span>053-850-6570</p>
+              </div>
             </div>
-            <div className="footer-info">
-              <p><span>Location</span>경상북도 경산시 진량읍 대구대로 201 정보통신대학 컴퓨터정보공학부 컴퓨터공학전공</p>
-              <p><span>TEL</span>053-850-6570</p>
-            </div>
-          </div>
-          <div className="footer-r">
-
           </div>
         </div>
       </footer>
