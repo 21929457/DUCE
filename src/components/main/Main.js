02f0d6d8914da1b0891f9f6ef15b0feda539659b
21929457 <apple855005@gmail.com>
2024-09-04 15:58:42 +0900

    수정

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,7 +1,6 @@
 import React, { useState , useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import $ from 'jquery';
 
 import '../../css/layout.scss';
 import Banner from './Banner';
