20f67fb08f308f2ad7d81e0bda24fbbde60fd7e3
21929457 <apple855005@gmail.com>
2024-09-05 16:12:01 +0900

    파일 초기화

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import './css/initialized.scss'
-import './css/App.css';
-import Header from "./components/Header"
-import Main from "./components/./main/Main"
-import Footer from "./components/Footer"
-
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Main/>
-      <Footer/>
+      
     </div>
   );
 }
