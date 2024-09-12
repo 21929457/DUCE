11636cec569fae01b0b7c515dde619577f159213
21929457 <apple855005@gmail.com>
2024-09-12 14:06:00 +0900

    fullpage 레이아웃 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import {SectionsContainer, Section} from 'react-fullpage';
+import Header from './components/Header';
 
 function App() {
+  let options = {
+    anchors: ['sectionOne', 'sectionTwo', 'sectionThree' , 'sectionfourth'],
+    navigation: false,
+};
   return (
     <div>
+      <Header/>
+      <SectionsContainer {...options}>
+        <Section>section1</Section>
+        <Section>section2</Section>
+        <Section>section3</Section>
+        <Section>section4</Section>
+      </SectionsContainer>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '../css/scss/header.scss';
+
+function Header (){
+    return (
+        <header>
+            
+        </header>
+    )
+}
+
+export default Header;
