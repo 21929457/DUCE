fec7df1dba9336800d49d7ab076dcd9ab437824e
21929457 <apple855005@gmail.com>
2024-09-13 13:41:01 +0900

    섹션 이동 처리

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ function Header (){
     return (
         <header>
             <nav>
-                <a href="#sectionTwo">전공안내</a>
+                <a href="#major">전공안내</a>
                 <a href="#">입학안내</a>
                 <a href="#">공지사항</a>
-                <a href="#">커뮤니티</a>
-                <a href="#">News Letter</a>
+                <a href="#studnetActivities">학생활동</a>
+                <a href="#newsLetter">News Letter</a>
             </nav>
         </header>
     )
