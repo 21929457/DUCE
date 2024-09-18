2390c8e1cec8e236b4c726842335a527878bd319
dhLee <apple855005@gmail.com>
2024-09-18 20:49:38 +0900

    전공 섹션 배너 작업 완료

--- src/components/Major.js
+++ src/components/Major.js
@@ -1,6 +1,13 @@
-import '../css/component/major.css'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import SwiperCore from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import '../css/component/major.css';
 
-function Major () {
+function Major() {
+    SwiperCore.use([Autoplay]);
+
     return (
         <div className='major-wrapper'>
             <div className='inner'>
@@ -9,9 +16,40 @@ function Major () {
                     <p className='major-tit'>전공선택</p>
                 </div>
                 <div className='major-r'>
+                    <Swiper
+                        className="mySwiper"
+                        direction={'vertical'}
+                        autoplay={{ delay: 0 }}
+                        loop={true}
+                        modules={{ Autoplay }}
+                        slidesPerView={2}
+                        spaceBetween={60}
+                        speed={3000}
+                    >
+                        <SwiperSlide><img src="../images/major_AI.jpg" /></SwiperSlide>
+                        <SwiperSlide></SwiperSlide>
+                        <SwiperSlide></SwiperSlide>
+                    </Swiper>
+                    <Swiper
+                        className="mySwiper"
+                        direction={'vertical'}
+                        autoplay={{ delay: 0, disableOnInteraction: false, loop: true }}
+                        modules={{ Autoplay }}
+                        loop={true}
+                        slidesPerView={2}
+                        centeredSlides={true}
+                        spaceBetween={60}
+                        speed={3000}
+                    >
+                        <SwiperSlide></SwiperSlide>
+                        <SwiperSlide></SwiperSlide>
+                        <SwiperSlide></SwiperSlide>
+                        <SwiperSlide></SwiperSlide>
+                        <SwiperSlide></SwiperSlide>
+                    </Swiper>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
