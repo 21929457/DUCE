a19a445c5ae40b2c16f319e17fc06dbd8523cbc2
21929457 <apple855005@gmail.com>
2024-10-28 17:18:20 +0900

    헤더 경로 및 news 섹션 호버 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ function Header (){
         <header>
             <nav>
                 <a href="#major">전공안내</a>
-                <a href="#">입학안내</a>
-                <a href="#">공지사항</a>
+                <a href="https://ipsi.daegu.ac.kr/intro.html">입학안내</a>
+                <a href="https://ce.daegu.ac.kr/hakgwa_home/ce/sub.php?menu=page&menu_id=30">공지사항</a>
                 <a href="#studnetActivities">학생활동</a>
                 <a href="#newsLetter">뉴스레터</a>
             </nav>
